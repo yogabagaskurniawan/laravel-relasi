06b8eb16700194665854a418a51e7dcdf0ad6039
yogabagaskurniawan <yogabag4s@gmail.com>
2023-07-12 19:31:40 +0700

    instalasi login dan register

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adminUser = [
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+        ];
+
+        if (!User::where('email', $adminUser['email'])->exists()){
+            User::create($adminUser);
+        }
+    }
+}
